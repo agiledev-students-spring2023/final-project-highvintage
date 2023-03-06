75dcbb12f9b764809d0c72f95101fc8fa3b18308
ezmartini <em4047@nyu.edu>
2023-03-06 17:40:34 -0500

    created generic header

---
+++ front-end/src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfilePreview from "../components/ProfilePreview";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import GenericHeader from "../components/GenericHeader";
+
+export default function Search() {
+  return (
+    <>
+      <GenericHeader pageName="Search" />
+    </>
+  );
+}
