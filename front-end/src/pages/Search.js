3adad8349bbb097f20747332e48e2dec881fa163
ezmartini <em4047@nyu.edu>
2023-03-06 17:55:41 -0500

    added searchbar

--- front-end/src/pages/Search.js
+++ front-end/src/pages/Search.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ProfilePreview from "../components/ProfilePreview";
-import { FaLongArrowAltLeft } from "react-icons/fa";
 import GenericHeader from "../components/GenericHeader";
 
 export default function Search() {
   return (
     <>
       <GenericHeader pageName="Search" />
+      <form className="mt-12 p-5 w-full">
+        <label>
+          <input
+            type="text"
+            name="searchQuery"
+            placeholder="Search for accounts..."
+            className="ml-5 border w-80 mr-3 p-1"
+          />
+        </label>
+        <input type="submit" value={"Search"} className="bg-gray-400 p-1" />
+      </form>
     </>
   );
 }
