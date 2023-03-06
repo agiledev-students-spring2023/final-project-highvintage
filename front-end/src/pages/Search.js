e73cd2b85daed5e93ca7fedf277f2511b69cd2a5
ezmartini <em4047@nyu.edu>
2023-03-06 18:10:53 -0500

    added logic for keeping track of past queries

--- front-end/src/pages/Search.js
+++ front-end/src/pages/Search.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ProfilePreview from "../components/ProfilePreview";
 import GenericHeader from "../components/GenericHeader";
+import { useState } from "react";
 
 export default function Search() {
+  const [recent, setRecent] = useState([]);
+  const [query, setQuery] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+
+  const recentQueries = recent.map((pastQuery) => (
+    <div>
+      {" "}
+      <p> {pastQuery}</p>{" "}
+    </div>
+  ));
+
   return (
     <>
       <GenericHeader pageName="Search" />
@@ -13,9 +25,21 @@ export default function Search() {
             name="searchQuery"
             placeholder="Search for accounts..."
             className="ml-5 border w-80 mr-3 p-1"
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
           />
         </label>
-        <input type="submit" value={"Search"} className="bg-gray-400 p-1" />
+        <input
+          type="submit"
+          value={"Search"}
+          className="bg-gray-400 p-1"
+          onClick={(e) => {
+            e.preventDefault();
+            setRecent([...recent, query]);
+            setIsSearching(true);
+          }}
+        />
       </form>
     </>
   );
