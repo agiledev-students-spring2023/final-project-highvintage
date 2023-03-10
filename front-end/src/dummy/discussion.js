290e94b4de40dc4e88b9a2a3a20d99a4de86e050
Zackdan0227 <kz2028@nyu.edu>
2023-03-10 18:05:52 -0500

    added dummy discussion threads

---
+++ front-end/src/dummy/discussion.js
@@ -0,0 +1,32 @@
+export const dummyDicussions = [
+  {
+    username: "user93028394",
+    title: "I love this place!",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit ation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+  },
+  {
+    username: "thriftlover",
+    title: "Second Street is OVERRATED!",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit ation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+  },
+  {
+    username: "kate7725",
+    title: "WOW I found this new thrift shop it's amazing!",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit ation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+  },
+  {
+    username: "bruh007",
+    title: "CHECK THIS OUT NOW!",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit ation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+  },
+  {
+    username: "Dude007",
+    title: "I hate it...",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit ation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+  },
+  {
+    username: "vintageKiller111",
+    title: "what do you think about ...",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit ation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+  },
+];
