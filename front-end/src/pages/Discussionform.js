c421ab2a9e34e66a0f28d5298c82ed72a28d1ed0
Zackdan0227 <kz2028@nyu.edu>
2023-03-10 15:53:38 -0500

    setup route

---
+++ front-end/src/pages/Discussionform.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+
+
+export default function Dicussionform() {
+  return (
+    
+    <>
+      
+
+      
+    </>
+  );
+}
