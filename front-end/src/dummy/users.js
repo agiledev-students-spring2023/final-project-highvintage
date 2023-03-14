25e75ad356194b3899bdc1dd9538212dae2de076
rubykiim <sk7383@nyu.edu>
2023-03-14 15:26:08 -0500

    added post info

--- front-end/src/dummy/users.js
+++ front-end/src/dummy/users.js
@@ -5,53 +5,152 @@ export const dummyUsers = [
     username: "user93028394",
     photo:
       "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80",
+    posts: [
+      {
+        postId: 1,
+        postDate: 'Mar 23, 2023',
+        postLoc: 'bklyn thrift',
+        postMedia: ["https://ln-site.nyc3.cdn.digitaloceanspaces.com/public/prod/2021/08/image/metcha_OnlyThrift_internal_18-custom.min.jpg",
+          "https://ln-site.nyc3.cdn.digitaloceanspaces.com/public/prod/2021/08/image/metcha_OnlyThrift_internal_18-custom.min.jpg"],
+        postLike: '132',
+        postText: 'I love thrifting!'
+      }
+    ]
   },
   {
     id: 2,
     username: "thriftlover",
     photo:
       "https://images.unsplash.com/photo-1630208232589-e42b29428b19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1538&q=80",
+    posts: [
+        {
+          postId: 1,
+        postDate: 'Mar 23, 2023',
+        postLoc: '',
+          postMedia: ["https://images.squarespace-cdn.com/content/v1/5726544ef85082b93e0f14c1/1584913604452-VFGS28EV3HZ2QGZOOA6G/men+thrift+store?format=1000w",
+          "https://images.squarespace-cdn.com/content/v1/5726544ef85082b93e0f14c1/1584913604452-VFGS28EV3HZ2QGZOOA6G/men+thrift+store?format=1000w"],
+          postLike: '132',
+          postText: 'I love thrifting!'
+        }
+      ]
   },
   {
     id: 3,
     username: "kate7725",
     photo:
       "https://images.unsplash.com/photo-1622842182823-28bfbfba47e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fHByb2ZpbGUlMjBwaG90b3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
+    posts: [
+        {
+          postId: 1,
+        postDate: 'Mar 23, 2023',
+        postLoc: 'cool thrift',
+          postMedia: ["https://cdn.cliqueinc.com/posts/276545/winter-thrift-store-finds-276545-1548447304321-main.700x0c.jpg",
+          "https://cdn.cliqueinc.com/posts/276545/winter-thrift-store-finds-276545-1548447304321-main.700x0c.jpg"],
+          postLike: '132',
+          postText: 'I love thrifting!'
+        }
+      ]
   },
   {
     id: 4,
     username: "big_dinner",
     photo:
       "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1317&q=80",
+    posts: [
+        {
+          postId: 1,
+        postDate: 'Mar 23, 2023',
+        postLoc: 'hi thrift',
+          postMedia: ["https://i0.wp.com/theurbandarling.com/wp-content/uploads/2021/09/IMG_2812-3.jpg",
+          "https://i0.wp.com/theurbandarling.com/wp-content/uploads/2021/09/IMG_2812-3.jpg"],
+          postLike: '132',
+          postText: 'I love thrifting!'
+        }
+      ]
   },
   {
     id: 5,
     username: "user2309",
     photo:
       "https://images.unsplash.com/photo-1505628346881-b72b27e84530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+    posts: [
+        {
+          postId: 1,
+        postDate: 'Mar 23, 2023',
+        postLoc: 'great thrift',
+          postMedia: ["https://i0.wp.com/static1.squarespace.com/static/5a93aadf36099bbdddfe2c35/t/5ebbed4f3f8d853f2163bfb5/1589375352017/thrift+store+style+thrift+store+clothing+finds+.jpg?resize=360%2C480&ssl=1",
+          "https://i0.wp.com/static1.squarespace.com/static/5a93aadf36099bbdddfe2c35/t/5ebbed4f3f8d853f2163bfb5/1589375352017/thrift+store+style+thrift+store+clothing+finds+.jpg?resize=360%2C480&ssl=1"],
+          postLike: '132',
+          postText: 'I love thrifting!'
+        }
+      ]
   },
   {
     id: 6,
     username: "user30943",
     photo:
       "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1200&q=60",
+    posts: [
+        {
+          postId: 1,
+        postDate: 'Mar 23, 2023',
+        postLoc: 'thrift cool',
+          postMedia: ["https://images.squarespace-cdn.com/content/v1/5726544ef85082b93e0f14c1/1594409397034-493NXIBKFQZO7STH338L/second+hand+stores?format=1000w",
+          "https://images.squarespace-cdn.com/content/v1/5726544ef85082b93e0f14c1/1594409397034-493NXIBKFQZO7STH338L/second+hand+stores?format=1000w"],
+          postLike: '132',
+          postText: 'I love thrifting!'
+        }
+      ]
   },
   {
     id: 7,
     username: "iloveshopping",
     photo:
       "https://images.unsplash.com/photo-1670272504471-61a632484750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      posts: [
+        {
+          postId: 1,
+          postDate: 'Mar 23, 2023',
+          postLoc: 'vintage cool',
+          postMedia: ["https://pbs.twimg.com/media/FNbwlvGVIAQYMhg.jpg",
+          "https://pbs.twimg.com/media/FNbwlvGVIAQYMhg.jpg"],
+          postLike: '132',
+          postText: 'I love thrifting!'
+        }
+      ]
   },
   {
     id: 8,
     username: "awesomesauce8384",
     photo:
       "https://images.unsplash.com/photo-1467043237213-65f2da53396f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2xvdGhlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1200&q=60",
+      posts: [
+        {
+          postId: 1,
+          postDate: 'Mar 23, 2023',
+          postLoc: 'yes thrift',
+          postMedia: ["https://i.pinimg.com/736x/53/06/f8/5306f88a58b8577fa57675ad83b1771c.jpg",
+          "https://i.pinimg.com/736x/53/06/f8/5306f88a58b8577fa57675ad83b1771c.jpg"],
+          postLike: '132',
+          postText: 'I love thrifting!'
+        }
+      ]
   },
   {
     id: 9,
     username: "krunker",
     photo:
       "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      posts: [
+        {
+          postId: 1,
+          postDate: 'Mar 23, 2023',
+          postLoc: 'best thrift',
+          postMedia: ["https://cdn.lookastic.com/looks/olive-long-sleeve-t-shirt-khaki-dress-pants-white-high-top-sneakers-large-59230.jpg",
+          "https://cdn.lookastic.com/looks/olive-long-sleeve-t-shirt-khaki-dress-pants-white-high-top-sneakers-large-59230.jpg"],
+          postLike: '132',
+          postText: 'I love thrifting!'
+        }
+      ]
   },
 ];
