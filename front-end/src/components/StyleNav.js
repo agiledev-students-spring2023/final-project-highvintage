c6a10de587133fae7a7a60991ab3f6735ae478fc
rubykiim <sk7383@nyu.edu>
2023-03-14 13:37:20 -0500

    smaller bottom margin

--- front-end/src/components/StyleNav.js
+++ front-end/src/components/StyleNav.js
@@ -7,7 +7,7 @@ export default function StyleNav() {
     
     return (
         <div>
-            <nav className="flex my-16 sm:justify-center space-x-6 px-2 bg-white text-center overflow-x-auto">
+            <nav className="flex mt-16 mb-4 sm:justify-center space-x-6 px-2 bg-white text-center overflow-x-auto">
                 {styles.map((style) => (
                         <button className="rounded-lg px-3 py-2 h-max my-auto text-slate-700 font-semibold 
                                         active:underline active:underline-offset-4 focus:underline focus:underline-offset-4 hover:bg-slate-100 hover:text-slate-900">
