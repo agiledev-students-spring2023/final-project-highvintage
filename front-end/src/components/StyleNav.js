1198776d3951da587472a83f963d02933cb65e3e
rubykiim <sk7383@nyu.edu>
2023-03-14 11:47:05 -0500

    underline on click

--- front-end/src/components/StyleNav.js
+++ front-end/src/components/StyleNav.js
@@ -1,4 +1,6 @@
 import React from 'react'
+// import React, { useState } from 'react'
+
 
 export default function StyleNav() {
     const styles = ['All', 'Sporty & Athleisure', 'Streetwear', 'Classic', 'Funk', 'Minimal']
@@ -7,7 +9,10 @@ export default function StyleNav() {
         <div>
             <nav className="flex my-16 sm:justify-center space-x-6 px-2 bg-white text-center overflow-x-auto">
                 {styles.map((style) => (
-                    <div className="rounded-lg px-3 py-2 my-auto text-slate-700 font-semibold active:underline active:underline-offset-4 hover:bg-slate-100 hover:text-slate-900">{style}</div>
+                        <button className="rounded-lg px-3 py-2 h-max my-auto text-slate-700 font-semibold 
+                                        active:underline active:underline-offset-4 focus:underline focus:underline-offset-4 hover:bg-slate-100 hover:text-slate-900">
+                            {style}
+                        </button>
                 ))}
             </nav>
         </div>
