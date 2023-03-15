da5c39d3905f379412ab7d4351583b3d3f199c71
rubykiim <sk7383@nyu.edu>
2023-03-15 15:49:43 -0400

    main navbar added, page number bar commented out

--- front-end/src/pages/OutfitCollection.js
+++ front-end/src/pages/OutfitCollection.js
@@ -2,30 +2,36 @@ import React from 'react'
 import { useState } from "react";
 import { FaLongArrowAltLeft } from "react-icons/fa";
 import GenericHeader from "../components/GenericHeader";
+import MainNav from '../components/MainNav';
 import StyleNav from '../components/StyleNav';
 import OutfitPreview from '../components/OutfitPreview';
+import PageNum from '../components/PageNum';
 import { dummyUsers } from '../dummy/users';
 
 export default function OutfitCollection() {
   // 6 previews per page
-  const dummyNew = dummyUsers.filter(user => user.id <= 6)
+  // const dummyNew = dummyUsers.filter(user => user.id <= 6)
 
-  const [users, setUsers] = useState(dummyNew);
+  const [users, setUsers] = useState(dummyUsers);
 
   const OutfitPreviews = users.map((user) => (
-    <OutfitPreview key={user.id} photo={user.posts[0].postMedia[0]} /> // show the first photo as preview
+    <OutfitPreview key={user.id} id={user.id} photo={user.posts[0].postMedia[0]} /> // show the first photo as preview
   ));
 
   return (
     <div>
+
       <GenericHeader pageName = "Outfits" />
       <StyleNav></StyleNav>
       
       {/* outfit grid */}
-      <div className='grid grid-cols-2 grid-rows-3 gap-1'>
+      <div className='grid grid-cols-2 gap-1'>
         {OutfitPreviews}
       </div>
 
+      {/* <PageNum /> */}
+      
+      <MainNav/>
     </div>
    
   )
