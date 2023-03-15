34412a5059dbb9d6098946cb8a82fbe591aee69c
rubykiim <sk7383@nyu.edu>
2023-03-15 15:48:31 -0400

    link added

--- front-end/src/components/OutfitPreview.js
+++ front-end/src/components/OutfitPreview.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function OutfitPreview(props) {
   const handleOnClick = () => {
-    console.log('..will lead to full post')
+    console.log(props.id)
+    const id = props.id
+    return id
   }
+
+  // individual preview within the grid - appears in OutfitCollection page
   return (
-      <div className='w-full h-full'>
-          <img
-            className="object-cover aspect-square justify-self-end"
-            src={props.photo}
-            onClick={handleOnClick}
+    <div className='w-full h-full'>
+
+      <Link to='/outfit-view'>
+        <img
+          className="object-cover aspect-square justify-self-end"
+          src={props.photo}
+          // onClick={handleOnClick}
           />
+      </Link>
+          
       </div>
   )
 }
