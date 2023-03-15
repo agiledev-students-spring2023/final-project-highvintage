edd36eaf0d8f5b35465833ea23255a356d084850
rubykiim <sk7383@nyu.edu>
2023-03-15 15:50:34 -0400

    moved to OutfitPost folder

--- front-end/src/components/OutfitText.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function OutfitText(props) {
-  return (
-    <div className='my-2 px-2'>
-      <p className='font-medium'>{props.text}</p>
-    </div>
-  )
-}
