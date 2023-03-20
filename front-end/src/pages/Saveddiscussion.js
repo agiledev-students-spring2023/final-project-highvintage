f4f5bdaee3c480f63aa06a313af81a9bccd025c1
Zackdan0227 <kz2028@nyu.edu>
2023-03-20 17:11:52 -0400

    added MainNav

--- front-end/src/pages/Saveddiscussion.js
+++ front-end/src/pages/Saveddiscussion.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import GenericHeader from "../components/GenericHeader";
+import MainNav from "../components/MainNav";
 import DiscussionPreviewFull from "../components/Saved/DiscussionPreviewFull";
 import SavedHeader from "../components/Saved/SavedHeader";
 import { dummyDicussions } from "../dummy/discussions";
-
 
 export default function Saveddiscussion() {
   const [discussions, setDiscussions] = useState(dummyDicussions);
@@ -18,8 +18,9 @@ export default function Saveddiscussion() {
     <>
       <GenericHeader pageName="Saved"></GenericHeader>
       <SavedHeader pageName="Discussion"></SavedHeader>
-      <div className = "pt-3"></div>
+      <div className="pt-3"></div>
       <div className="grid grid-cols-1 gap-3 my-1 ">{discussionComponents}</div>
+      <MainNav></MainNav>
     </>
   );
 }
