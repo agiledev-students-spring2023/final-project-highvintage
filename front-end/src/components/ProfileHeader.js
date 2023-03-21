602425e7cec5debe01106bfa4b08750100df38b1
“katepilled” <katehyoon@gmail.com>
2023-03-20 20:43:34 -0400

    allowed for edit profile button option

--- front-end/src/components/ProfileHeader.js
+++ front-end/src/components/ProfileHeader.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 /**
  * A React component that represents a user's profile header
  * @returns The contents of this component, in JSX form.
  */
 export default function ProfileHeader(props) {
+  const [selfProfile, setSelfProfile] = useState(true);
   const {
     username,
     profilePicture,
@@ -28,10 +29,17 @@ export default function ProfileHeader(props) {
         <div className="flex flex-col">
           <div className="flex flex-row items-center mt-4">
             <h2 className="text-2xl font-bold">{username}</h2>
-            <button className="bg-gray-300 text-black px-4 py-2 rounded-lg ml-8">
-              Follow
-            </button>
+            {selfProfile ? (
+              <button className="bg-gray-300 text-black px-4 py-2 rounded-lg ml-8">
+                Edit Profile
+              </button>
+            ) : (
+              <button className="bg-gray-300 text-black px-4 py-2 rounded-lg ml-8">
+                Follow
+              </button>
+            )}
           </div>
+  
           <ul className="flex flex-row mt-4">
             <li className="mr-6">
               <span className="font-bold">{posts}</span> posts
