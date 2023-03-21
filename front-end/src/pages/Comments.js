fde3354ff65388a1619f26805c615bc23c8cc3d4
ezmartini <em4047@nyu.edu>
2023-03-21 19:03:18 -0400

    comments page setup

---
+++ front-end/src/pages/Comments.js
@@ -0,0 +1,10 @@
+import React from "react";
+import GenericHeader from "../components/GenericHeader";
+
+export default function Comments() {
+  return (
+    <>
+      <GenericHeader pageName="Comments" />
+    </>
+  );
+}
