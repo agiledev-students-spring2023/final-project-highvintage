b1514fb47b83b277f34cad40443dec0229130fcc
ezmartini <em4047@nyu.edu>
2023-03-21 19:56:41 -0400

    Created and styled comments

--- front-end/src/components/Comment.js
+++ front-end/src/components/Comment.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { dummyComments } from "../dummy/comments";
 
-export default function Comment() {
-  return <div>Comment</div>;
+export default function Comment(props) {
+  // keep from accessing many props all the time
+  const user = props.user;
+  return (
+    <>
+      <div className="grid grid-cols-12 p-3 bg-white mb-3">
+        <div className="col-span-2">
+          {" "}
+          <img className="h-20 object-cover aspect-square" src={user.photo} />
+        </div>
+
+        <div className="col-span-9 ml-4">
+          {" "}
+          <p className="align-center font-semibold"> @{user.username} </p>
+          <p> {props.body} </p>
+        </div>
+      </div>
+    </>
+  );
 }
--- front-end/src/pages/Comments.js
+++ front-end/src/pages/Comments.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import GenericHeader from "../components/GenericHeader";
 import { dummyComments } from "../dummy/comments";
+import Comment from "../components/Comment";
 
 export default function Comments() {
+  const [comments, setComments] = useState(dummyComments);
+
+  const commentComponents = comments.map((comment) => (
+    <Comment user={comment.user} key={comment.id} body={comment.body} />
+  ));
   return (
     <>
       <GenericHeader pageName="Comments" />
+
+      <section className="mt-16 w-10/12 mr-auto ml-auto">
+        {commentComponents}
+      </section>
     </>
   );
 }
