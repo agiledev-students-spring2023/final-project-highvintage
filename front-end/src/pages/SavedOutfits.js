b7de5b14e72fcd3d96293948783ac8a3d195d4ad
“katepilled” <katehyoon@gmail.com>
2023-03-21 17:44:51 -0400

    added dummy posts

--- front-end/src/pages/SavedOutfits.js
+++ front-end/src/pages/SavedOutfits.js
@@ -2,14 +2,27 @@ import React, { useState } from "react";
 import GenericHeader from "../components/GenericHeader";
 import MainNav from "../components/MainNav";
 import SavedHeader from "../components/Saved/SavedHeader";
+import OutfitPreview from "../components/OutfitPreview";
+import { dummyUsers } from "../dummy/users";
 
 export default function SavedOutfits() {
+const [users, setUsers] = useState(dummyUsers);
+
+  const OutfitPreviews = users.map((user) => (
+    <OutfitPreview
+      key={user.id}
+      id={user.id}
+      photo={user.posts[0].postMedia[0]}
+    />
+  ));
+
+ 
   return (
     <>
       <GenericHeader pageName="Saved"></GenericHeader>
       <SavedHeader pageName="Outfits"></SavedHeader>
-      <div className="grid grid-cols-1 gap-3 my-1 mt-2 mb-16">
-      </div>
+      {/* outfit grid */}
+      <div className="grid grid-cols-3 gap-1">{OutfitPreviews}</div>
       <MainNav></MainNav>
     </>
   );
