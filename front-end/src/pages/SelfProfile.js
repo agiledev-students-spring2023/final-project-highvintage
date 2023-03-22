55304278b492ec506842e11a63c67b4b9f48ad66
katepilled <katehyoon@gmail.com>
2023-03-22 18:26:10 -0400

    added new comp to show posts

--- front-end/src/pages/SelfProfile.js
+++ front-end/src/pages/SelfProfile.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import ProfileHeader from "../components/ProfileHeader.js";
+import ProfileHeader from "../components/Profile/ProfileHeader.js";
+import ProfileShowPosts from "../components/Profile/ProfileShowPosts.js";
 import OutfitPreview from "../components/OutfitPreview.js";
 import GenericHeader from "../components/GenericHeader.js";
 import { dummyUsers } from "../dummy/users";
@@ -53,25 +54,8 @@ const SelfProfile = () => {
   return (
     <div className="mb-16">
       <GenericHeader pageName="Profile"></GenericHeader>
-
       {headerComp}
-
-      {/*Outfit posts / Discussion bar */}
-      <ul class="flex flex-row mt-4 font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadowdark:divide-gray-700 dark:text-gray-400">
-        <li class="flex-1">
-          <button class="inline-block w-full p-4 text-gray-900 bg-gray-200 dark:bg-gray-700 dark:text-white">
-            Outfits
-          </button>
-        </li>
-        <li class="flex-1">
-          <button class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">
-            Discussion Posts
-          </button>
-        </li>
-      </ul>
-
-      {/* outfit grid */}
-      <div className="grid grid-cols-3 gap-1">{OutfitPreviews}</div>
+      <ProfileShowPosts></ProfileShowPosts>
       <MainNav></MainNav>
     </div>
   );
