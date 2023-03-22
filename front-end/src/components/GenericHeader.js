0f3e15c6916e6f260f3d4738a1ec8b36e15c2a2a
wjdgus02 <blackbirdkjh@gmail.com>
2023-03-22 00:11:18 -0400

    home

--- front-end/src/components/GenericHeader.js
+++ front-end/src/components/GenericHeader.js
@@ -5,15 +5,20 @@ export default function GenericHeader(props) {
   return (
     <>
       {" "}
-      <div className="fixed bg-white top-0 left-0 right-0 w-full grid-cols-2 p-3">
+      <div className="fixed bg-white top-0 left-0 right-0 w-full flex justify-between p-3">
         {" "}
         <FaLongArrowAltLeft />
         
-        <h1 className="-mt-5 text-center text-2xl font-extrabold">
+        <h1 className="mt-2 text-center text-2xl font-extrabold">
           {" "}
           {props.pageName}
         </h1>{" "}
-    
+
+        <div className="flex items-center">
+        <a href="/search" className="text-gray-500 hover:text-gray-700">
+          <i className="fas fa-search"></i>
+        </a>
+      </div>
       </div>
     </>
   );
--- front-end/src/components/OutfitPost/OutfitPost.js
+++ front-end/src/components/OutfitPost/OutfitPost.js
@@ -19,12 +19,12 @@ export default function OutfitPost() {
 
   return (
     // full outfit post - 4 children components combined
-    <div className='grid grid-row-4 py-2 bg-white rounded-lg'>
+    <div className='grid grid-row-4 py-2 bg-white rounded-lg shadow-md'>
 
       <OutfitUserInfo photo={users[0].photo} username={users[0].username} location={users[0].posts[0].postLoc} postDate={users[0].posts[0].postDate} />
       
 
-      <div className='flex flex-row overflow-x-auto '>
+      <div className='flex flex-row overflow-x-auto justify-self-center'>
         <OutfitMedia media={users[0].posts[0].postMedia} />
         {/* {mediaComponent} */}
       </div>
