e3a3785fb7051dc684b47f4d5cc9904297c66f6f
Zackdan0227 <kz2028@nyu.edu>
2023-03-28 12:31:37 -0400

    added like, comment, save buttons

--- front-end/src/components/Saved/DiscussionFullView.js
+++ front-end/src/components/Saved/DiscussionFullView.js
@@ -1,21 +1,78 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {
+  FaRegHeart,
+  FaHeart,
+  FaRegCommentDots,
+  FaRegBookmark,
+  FaBookmark,
+} from "react-icons/fa";
 export default function DiscussionFullView(props) {
+  const navigate = useNavigate();
+
+  const handleLike = () => {
+    console.log("handle like");
+  };
+  const handleComment = () => {
+    console.log("handle comment");
+  };
+  const handleSave = () => {
+    console.log("handle save");
+  };
   return (
     <div className="w-full max-w-md mx-auto pt-10 mt-5 bg-white shadow-md rounded-md overflow-hidden">
       <div className="flex items-center px-4 py-2 border-b border-gray-200">
-        <h2 className="text-lg font-serif leading-none font-bold text-gray-800 mr-auto">{props.title}</h2>
+        <h2 className="text-lg font-serif leading-none font-bold text-gray-800 mr-auto">
+          {props.title}
+        </h2>
         <div className="flex items-center">
-          <img src={props.photo} alt="User Avatar" className="w-8 h-8 rounded-full mr-2" />
-          <span className="overflow-hidden truncate text-gray-600">{props.username}</span>
+          <img
+            src={props.photo}
+            alt="User Avatar"
+            className="w-8 h-8 rounded-full mr-2"
+          />
+          {/* need link to the profile when click on */}
+          <span className="overflow-hidden truncate text-gray-600">
+            {props.username}
+          </span>
         </div>
       </div>
+      <Link to="/discussion-view">
       <div className="p-4">
         <p className="text-gray-700 leading-relaxed text-justify">
-            {props.content}
+          {props.content}
         </p>
-        <div className="text-right text-gray-400 text-sm mt-4">{props.date.toLocaleDateString()}</div>
+      </div>
+      </Link>
+      
+      <div className="flex flex-row">
+        <div className="grid grid-cols-2 px-2">
+          <div className="flex space-x-2">
+            {/* Like */}
+            <div className="my-auto" onClick={handleLike}>
+              <FaRegHeart size={25} />
+            </div>
+
+            {/* Comment */}
+            <div className="my-auto" onClick={handleComment}>
+              <FaRegCommentDots
+                size={24}
+                onClick={() => navigate("/comments")}
+              />
+            </div>
+          </div>
+
+          <div className="my-auto ml-2" onClick={handleSave}>
+            {/* Save */}
+            <FaRegBookmark size={24} />
+          </div>
+        </div>
+        <div className="justify-self-end ml-10 "></div>
+        <div className="text-right text-gray-400 text-sm ml-28 mt-4 mb-2">
+          {props.date.toLocaleDateString()}
+        </div>
       </div>
     </div>
-  )
+  );
 }
