0d525cdb86483d5ad5255e389ff57d3cbf1c8528
ezmartini <em4047@nyu.edu>
2023-03-29 13:25:51 -0400

    add route

--- back-end/routes/users.mjs
+++ back-end/routes/users.mjs
@@ -6,4 +6,13 @@ router.get("/", function (req, res) {
   res.send("hello user!");
 });
 
+router.get("/me", function (req, res) {
+  if (!req.user) {
+    // unauthenticated
+    res.statusCode(401);
+  } else {
+    res.status(200).json(req.user);
+  }
+});
+
 export default router;
