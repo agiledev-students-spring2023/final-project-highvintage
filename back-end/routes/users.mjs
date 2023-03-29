2dc3c055c5b1ea77883a019c6f6152cce970a5ca
ezmartini <em4047@nyu.edu>
2023-03-29 16:29:14 -0400

    adding call on front end on page load

--- back-end/routes/users.mjs
+++ back-end/routes/users.mjs
@@ -6,16 +6,20 @@ const router = express.Router();
 router.get("/:username", function (req, res) {
   // input is cleaned in front end, before call is made
   const foundUser = dummyUsers.find(
-    (user) => user.username == req.params.username
+    (user) => user.username == req.params.username.toLowerCase()
   );
 
   if (!foundUser) {
-    return res.json({ status: 401, message: "Unkown User ID" });
+    return res.json({ status: 401, message: "Unknown User ID" });
   } else {
     return res.json({
       status: 200,
       user: {
-        isMe: req.user.username == req.params.username,
+        isSelf: req.user.username == req.params.username,
+        profilePicture: foundUser.photo,
+        style: "Streetwear",
+        favoriteThrift: "L Train Vintage",
+        bio: "I love clothes!",
         username: foundUser.username,
         posts: foundUser.posts,
         discussion: foundUser.discussion,
