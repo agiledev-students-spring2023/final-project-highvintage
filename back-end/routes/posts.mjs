d4fe66e5da3b920f907e917a8b2e2796bef4ed63
ezmartini <em4047@nyu.edu>
2023-04-01 21:07:33 -0400

    fetching response from backend

--- back-end/routes/posts.mjs
+++ back-end/routes/posts.mjs
@@ -1,5 +1,4 @@
 import express from "express";
-import dummyUsers from "../mock-db/mock.mjs";
 import dummyPosts from "../mock-db/mock_posts.mjs";
 
 const router = express.Router();
--- front-end/src/pages/OutfitView.js
+++ front-end/src/pages/OutfitView.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import OutfitPost from '../components/OutfitPost/OutfitPost'
-import GenericHeader from '../components/GenericHeader'
+import React, { useEffect, useState } from "react";
+import OutfitPost from "../components/OutfitPost/OutfitPost";
+import GenericHeader from "../components/GenericHeader";
+import { requestURL } from "../requestURL";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export default function OutfitView() {
-    return (
-      // page to view full outfit post
-      <div>
-          <GenericHeader pageName="View Post"></GenericHeader>
-          <div className='mt-20'>
-              <OutfitPost></OutfitPost>
-          </div>
+  // fetch on page load - useEffect
+  const params = useParams();
+  const [post, setPost] = useState({
+    postId: 0,
+    date: "",
+    postLoc: "",
+    postMedia: [],
+    postLike: [],
+    postText: "",
+    comments: [],
+    author: "",
+  });
+  useEffect(() => {
+    async function fetchPost(query) {
+      const response = await axios.get(requestURL + "posts/view/" + query);
+      return response.data;
+    }
+
+    fetchPost(params.id).then((res) => setPost(res));
+
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <GenericHeader pageName="View Post"></GenericHeader>
+      <div className="mt-20">
+        <OutfitPost post={post}></OutfitPost>
       </div>
-  )
+    </div>
+  );
 }
