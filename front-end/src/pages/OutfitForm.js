6ed6bee10e428b54928040e74ddfd64b517390c6
rubykiim <sk7383@nyu.edu>
2023-04-03 02:46:22 -0400

    handle file upload & show created post info (temp)

--- front-end/src/pages/OutfitForm.js
+++ front-end/src/pages/OutfitForm.js
@@ -9,10 +9,26 @@ import { requestURL } from "../requestURL";
 export default function OutfitForm() {
   const styles = dummyStyles;
   const [success, setSuccess] = useState(null);
+  const [post, setPost] = useState(null);
 
-  const onSubmit = async (values, { resetForm }) => {
+  post && console.log("post", post);
+
+  const onSubmit = async (values, { resetForm, setFieldValue }) => {
+    const formData = new FormData();
+    formData.append("content", values.content);
+    formData.append("location", values.location);
+    formData.append("style", values.style);
+    // append each uploaded file to the FormData object
+    for (let i = 0; i < values.my_files.length; i++) {
+      formData.append("my_files", values.my_files[i]);
+    }
+
     const response = await axios
-      .post(requestURL + "posts/create", values)
+      .post(requestURL + "posts/create", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
       .catch((err) => {
         if (err && err.response) {
           console.log("Error :", err);
@@ -22,7 +38,9 @@ export default function OutfitForm() {
       console.log("values", values);
       console.log("response.data", response.data);
       setSuccess(response.data.message);
+      setFieldValue("my_files", []); // clear the file input field.. not working yet
       resetForm(); // Reset the form after successful submission
+      setPost(response.data.newPost);
     }
   };
 
@@ -31,6 +49,7 @@ export default function OutfitForm() {
       style: "Sporty & Athleisure",
       location: "",
       content: "",
+      my_files: [],
     },
     onSubmit,
   });
@@ -38,7 +57,6 @@ export default function OutfitForm() {
   // console.log(formik.values);
   return (
     <div className="relative">
-
       {/* Header */}
       <div className="absolute z-30">
         <GenericHeader pageName="Share Outfits" />
@@ -46,20 +64,43 @@ export default function OutfitForm() {
 
       {/* Success Message */}
       {success && (
-        <div className="absolute inset-0 z-20 flex justify-center items-center">
+        <div className="absolute inset-0 z-20 flex flex-col justify-center items-center ">
           <OutfitPostMsg
             success={success ? success : ""}
             className="z-20"
           ></OutfitPostMsg>
+          {/* just to verify that post is created - to be deleted */}
+          <div className="grid grid-rows-3 mt-4 bg-orange-100 p-4 rounded-lg">
+            {post.files.my_files[0].filename && (
+              <div>{`FILES: ${post.files.my_files[0].filename}`}</div>
+            )}
+            <div>{`STYLE: ${post.style}`}</div>
+            <div>{`CONTENT: ${post.content}`}</div>
+          </div>
         </div>
       )}
 
       {/* Form */}
-      <div className="flex justify-center items-center h-screen relative z-0">
+      <div className="flex justify-center items-center relative z-0 h-screen">
         <form
           onSubmit={formik.handleSubmit}
-          className="bg-white p-10 rounded-lg"
+          className="bg-white p-10 rounded-lg my-auto"
         >
+          {/* File Upload */}
+          <div className="mb-4">
+            <label htmlFor="upload" className="text-gray-700 font-bold mb-2">
+              Upload
+            </label>
+            <input
+              type="file"
+              name="my_files"
+              className="pl-6"
+              onChange={(e) => {
+                formik.setFieldValue("my_files", e.currentTarget.files);
+              }}
+              multiple
+            />
+          </div>
           {/* Style */}
           <div className="mb-4">
             <label
@@ -75,7 +116,9 @@ export default function OutfitForm() {
               onChange={formik.handleChange}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option selected>Select Style</option>
+              <option disabled value="">
+                Select Style
+              </option>
               {styles.map((style, index) => {
                 if (style !== "All") {
                   return <option key={index}>{style}</option>;
@@ -99,7 +142,9 @@ export default function OutfitForm() {
               onChange={formik.handleChange}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option selected>Add Location</option>
+              <option disabled value="">
+                Add Location
+              </option>
               {/* Integrate Gmaps */}
             </select>
           </div>
