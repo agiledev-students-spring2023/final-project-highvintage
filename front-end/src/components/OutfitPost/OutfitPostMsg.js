0a58dcdce3ba026b644cf784bd6923c86057a8be
rubykiim <sk7383@nyu.edu>
2023-04-03 02:46:34 -0400

    updated style

--- front-end/src/components/OutfitPost/OutfitPostMsg.js
+++ front-end/src/components/OutfitPost/OutfitPostMsg.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function OutfitPostMsg(props) {
   return (
-    <div className='bg-slate-500 color-white'>{props.success}</div>
+    <div className='bg-gray-400 text-white font-semibold text-lg px-3 py-2 rounded-lg'>{props.success}</div>
   )
 }
