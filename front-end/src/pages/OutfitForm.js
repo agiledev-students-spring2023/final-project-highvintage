b762a1da257646ed4cf0ed57440353ec03fe0eaf
rubykiim <sk7383@nyu.edu>
2023-04-02 23:06:49 -0400

    correctly display success msg

--- front-end/src/pages/OutfitForm.js
+++ front-end/src/pages/OutfitForm.js
@@ -38,82 +38,93 @@ export default function OutfitForm() {
   return (
     <>
       <GenericHeader pageName="Share Outfits" />
-      
-      <div className="flex justify-center items-center h-screen">
-      <OutfitPostMsg success={success ? success : ""} className=""></OutfitPostMsg>
-        {/* Form Start */}
-        <form
-          onSubmit={formik.handleSubmit}
-          className="bg-white p-10 rounded-lg "
-        >
-          {/* Style */}
-          <div className="mb-4">
-            <label
-              htmlFor="style"
-              className="block text-gray-700 font-bold mb-2"
-            >
-              Style
-            </label>
-            <select
-              name="style"
-              required
-              value={formik.values.style}
-              onChange={formik.handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            >
-              <option selected>Select Style</option>
-              {styles.map((style, index) => {
-                if (style !== "All") {
-                  return <option key={index}>{style}</option>;
-                }
-              })}
-            </select>
-          </div>
 
-          {/* Location */}
-          <h2 className="text-2xl font-bold mb-4"></h2>
-          <div className="mb-4">
-            <label
-              htmlFor="style"
-              className="block text-gray-700 font-bold mb-2"
-            >
-              Location
-            </label>
-            <select
-              name="location"
-              value={formik.values.location}
-              onChange={formik.handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            >
-              <option selected>Add Location</option>
-              {/* Integrate Gmaps */}
-            </select>
-          </div>
+      <div className="relative">
 
-          {/* Content */}
-          <div className="mb-4">
-            <textarea
-              placeholder="Write a caption..."
-              name="content"
-              value={formik.values.content}
-              onChange={formik.handleChange}
-              className="w-full border border-gray-400 p-2 rounded-md"
-              rows="10"
-              cols="40"
-              required
-            ></textarea>
-          </div>
+        {success && (
+          <div className="absolute inset-0 z-20 flex justify-center items-center">
+            <OutfitPostMsg
+              success={success ? success : ""}
+              className=""
+            ></OutfitPostMsg>
+            </div>
+          )}
+        
+        {/* Form */}
+        <div className="flex justify-center items-center h-screen">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="bg-white p-10 rounded-lg relative z-10"
+          >
+            {/* Style */}
+            <div className="mb-4">
+              <label
+                htmlFor="style"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Style
+              </label>
+              <select
+                name="style"
+                required
+                value={formik.values.style}
+                onChange={formik.handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option selected>Select Style</option>
+                {styles.map((style, index) => {
+                  if (style !== "All") {
+                    return <option key={index}>{style}</option>;
+                  }
+                })}
+              </select>
+            </div>
 
-          {/* Post Button */}
-          <div className="flex justify-end">
-            <button
-              type="submit"
-              className="ml-auto bg-gray-400 text-white px-10 py-2 rounded-md hover:bg-gray-700"
-            >
-              Post
-            </button>
-          </div>
-        </form>
+            {/* Location */}
+            <h2 className="text-2xl font-bold mb-4"></h2>
+            <div className="mb-4">
+              <label
+                htmlFor="style"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Location
+              </label>
+              <select
+                name="location"
+                value={formik.values.location}
+                onChange={formik.handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option selected>Add Location</option>
+                {/* Integrate Gmaps */}
+              </select>
+            </div>
+
+            {/* Content */}
+            <div className="mb-4">
+              <textarea
+                placeholder="Write a caption..."
+                name="content"
+                value={formik.values.content}
+                onChange={formik.handleChange}
+                className="w-full border border-gray-400 p-2 rounded-md"
+                rows="10"
+                cols="40"
+                required
+              ></textarea>
+            </div>
+
+            {/* Post Button */}
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="ml-auto bg-gray-400 text-white px-10 py-2 rounded-md hover:bg-gray-700"
+              >
+                Post
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
