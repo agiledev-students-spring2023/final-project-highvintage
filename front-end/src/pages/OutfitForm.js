72b197941d546f1a6746ab99f64b4d19b39ece0c
rubykiim <sk7383@nyu.edu>
2023-04-04 13:29:28 -0400

    minor update & style change for file upload

--- front-end/src/pages/OutfitForm.js
+++ front-end/src/pages/OutfitForm.js
@@ -12,7 +12,7 @@ export default function OutfitForm() {
   const [post, setPost] = useState(null);
 
   post && console.log("post", post);
-  post && console.log("post.postMedia", post.postMedia)
+  post && console.log("post.postMedia", post.postMedia);
 
   const onSubmit = async (values, { resetForm, setFieldValue }) => {
     const formData = new FormData();
@@ -39,7 +39,7 @@ export default function OutfitForm() {
       console.log("values", values);
       console.log("response.data", response.data);
       setSuccess(response.data.message);
-      setFieldValue("my_files", []); // clear the file input field.. not working yet
+      // setFieldValue("my_files", []); // clear the file input field.. not working yet
       resetForm(); // Reset the form after successful submission
       setPost(response.data.newPost);
     }
@@ -73,10 +73,10 @@ export default function OutfitForm() {
           {/* just to verify that post is created - to be deleted */}
           <div className="grid grid-rows-3 mt-4 bg-orange-100 p-4 rounded-lg">
             {post.postMedia.my_files[0].filename && (
-              <div>{`FILES: ${post.postMedia.my_files[0].filename}`}</div>
+              <div>{post.postMedia.my_files[0].filename}</div>
             )}
-            <div>{`STYLE: ${post.style}`}</div>
-            <div>{`CONTENT: ${post.postText}`}</div>
+            <div>{`${post.style}`}</div>
+            <div>{`${post.postText}`}</div>
           </div>
         </div>
       )}
@@ -89,7 +89,7 @@ export default function OutfitForm() {
         >
           {/* File Upload */}
           <div className="mb-4">
-            <label htmlFor="upload" className="text-gray-700 font-bold mb-2">
+            {/* <label htmlFor="upload" className="text-gray-700 font-bold mb-2">
               Upload
             </label>
             <input
@@ -100,7 +100,32 @@ export default function OutfitForm() {
                 formik.setFieldValue("my_files", e.currentTarget.files);
               }}
               multiple
+            /> */}
+            <label
+              htmlFor="upload"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Add Photos
+            </label>
+            <input
+              type="file"
+              name="my_files"
+              id="upload"
+              className="relative m-0 rounded-lg block w-full min-w-0 flex-auto border border-solid border-neutral-300 bg-clip-padding py-[0.32rem] px-3 
+              text-sm font-normal text-neutral-700 file:-mx-3 file:-my-[0.32rem] 
+              file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 
+              file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none"
+              onChange={(e) => {
+                formik.setFieldValue("my_files", e.currentTarget.files);
+              }}
+              multiple
             />
+            {/* <label
+                htmlFor="upload"
+                className="block px-4 py-2 bg-green border border-gray-400 rounded-md cursor-pointer hover:border-gray-500"
+              >
+                <span className="truncate">Photo / Video</span>
+              </label> */}
           </div>
           {/* Style */}
           <div className="mb-4">
