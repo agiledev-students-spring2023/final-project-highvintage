bf37ed6d3c7ada7d3981dbedf24c1e0ecf4c9d91
rubykiim <sk7383@nyu.edu>
2023-04-16 21:21:39 -0400

    added style field

--- back-end/schemas/posts.js
+++ back-end/schemas/posts.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   author: { type: ObjectId, ref: "User", required: true }, // reference to author
+  style: { type: String, required: true },
   caption: { type: String, required: true },
   // photos
   comments: [{ type: ObjectId, ref: "Comment" }],
