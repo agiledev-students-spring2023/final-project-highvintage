3a37a2c0dd1efda635be9453c5fabe73eacd4524
katepilled <katehyoon@gmail.com>
2023-04-21 23:07:52 -0400

    save button fix

--- front-end/src/pages/EditProfile.js
+++ front-end/src/pages/EditProfile.js
@@ -160,14 +160,14 @@ export default function EditProfile() {
               <div className="text-center">
                 <button
                   type="submit"
-                  className={`bg-${
+                  className={
                     !update.username &&
                     !update.favoriteThrift &&
                     !update.style &&
                     !update.bio
-                      ? "gray-400"
-                      : '"blue-500"'
-                  } text-white font-bold py-2 px-4 rounded`}
+                      ? "bg-gray-400 text-white font-bold py-2 px-4 rounded"
+                      : "bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                  }
                   disabled={
                     !update.username &&
                     !update.favoriteThrift &&
