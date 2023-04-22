af4daf9059383ef09a4bdffa29f492041d0196e5
rubykiim <sk7383@nyu.edu>
2023-04-22 17:07:42 -0400

    working on img

--- front-end/src/pages/OutfitCollection.js
+++ front-end/src/pages/OutfitCollection.js
@@ -16,7 +16,6 @@ export default function OutfitCollection() {
   const [filteredPosts, setFilteredPosts] = useState([]);
   const [me, setMe] = useState("");
   const [style, setStyle] = useState("");
-
   const [allPosts, setAllPosts] = useState([]);
 
   const filterByStyle = useCallback((style) => {
@@ -39,89 +38,96 @@ export default function OutfitCollection() {
     }
   }, [allPosts]);
 
+  // function arrayBufferToBase64(buffer) {
+  //   var binary = '';
+  //   var bytes = [].slice.call(new Uint8Array(buffer));
+  //   bytes.forEach((b) => binary += String.fromCharCode(b));
+  //   return window.btoa(binary);
+  // };
+
   useEffect(() => {
     async function fetchCollection() {
       const response = await axios
         .get(requestURL + "posts/collection")
         .then(console.log("* Fetched"))
-        .catch((err) => {
-          if (err) {
-            console.log("Error :", err);
-          }
-        });
-      
-      console.log('response', response);
-
-      if (response) {
-        console.log("* Fetched Posts", response.data.allPosts);
-        setAllPosts(response.data.allPosts);
-        console.log('* setAllPosts result', allPosts)
+    .catch((err) => {
+      if (err) {
+        console.log("Error :", err);
       }
-    }
-    fetchCollection();
+    });
+
+  console.log('response', response);
+
+  if (response) {
+    console.log("* Fetched Posts", response.data.allPosts);
+    setAllPosts(response.data.allPosts);
+    console.log('* setAllPosts result', allPosts)
+  }
+}
+fetchCollection();
   }, []);
 
-  useEffect(() => {
-    if (allPosts.length > 0) {
-      filterByStyle(style);
-    }
-  }, [allPosts, style, filterByStyle]);
+useEffect(() => {
+  if (allPosts.length > 0) {
+    filterByStyle(style);
+  }
+}, [allPosts, style, filterByStyle]);
 
-  console.log('filteredPosts', filteredPosts);
+console.log('filteredPosts', filteredPosts);
 
-  const OutfitPreviews = filteredPosts.map((post) => (
-    post.photos.length > 0 &&
-    <OutfitPreview
-      key={post._id}
-      id={post._id}
-      photo={post.photos[0]} // first photo as preview
-    />
-  ));
+const OutfitPreviews = filteredPosts.map((post) => (
+  post.photos.length > 0 &&
+  <OutfitPreview
+    key={post._id}
+    id={post._id}
+    photo={post.photos[0]} // first photo as preview
+  />
+));
 
-  /*
-  // dummy user
-  const filterByStyle = (style) => {
-    console.log(style)
-    let newPosts
+/*
+// dummy user
+const filterByStyle = (style) => {
+  console.log(style)
+  let newPosts
 
-    style == 'All'
-      ? newPosts = users
-      : newPosts = users.filter((user) => user.posts[0].postStyle == style)
-    setFilteredPosts(newPosts)
-  };
+  style == 'All'
+    ? newPosts = users
+    : newPosts = users.filter((user) => user.posts[0].postStyle == style)
+  setFilteredPosts(newPosts)
+};
 
-  const OutfitPreviews = filteredPosts.map((user) => (
-    <OutfitPreview
-      key={user.id}
-      id={user.id}
-      photo={user.posts[0].postMedia[0]} // first photo as preview
-    /> 
-  ));
-  */
-  useEffect(() => {
-    async function fetchMe() {
-      const response = await axios.get(requestURL + "users/me");
-      setMe(response.data.user.username);
-    }
-    fetchMe();
-  });
+const OutfitPreviews = filteredPosts.map((user) => (
+  <OutfitPreview
+    key={user.id}
+    id={user.id}
+    photo={user.posts[0].postMedia[0]} // first photo as preview
+  /> 
+));
+*/
+useEffect(() => {
+  async function fetchMe() {
+    const response = await axios.get(requestURL + "users/me");
+    setMe(response.data.user.username);
+  }
+  fetchMe();
+});
 
-  return (
-    <div>
-      <GenericHeader pageName="Outfits" />
-      <StyleNav filterByStyle={filterByStyle}></StyleNav>
+return (
+  <div>
+    <GenericHeader pageName="Outfits" />
+    <StyleNav filterByStyle={filterByStyle}></StyleNav>
 
-      {/* outfit grid */}
-      <div className="grid grid-cols-2 gap-1">{OutfitPreviews}</div>
+    {/* outfit grid */}
+    <div className="grid grid-cols-2 gap-1">{OutfitPreviews}</div>
 
-      <button
-        onClick={() => navigate("/outfit-form")}
-        className="fixed bottom-4 left-0 mb-10 w-full text-l font-bold bg-gray-500 text-white py-3"
-      >
-        Post
-      </button>
+    <button
+      onClick={() => navigate("/outfit-form")}
+      className="fixed bottom-4 left-0 mb-10 w-full text-l font-bold bg-gray-500 text-white py-3"
+    >
+      Post
+    </button>
 
-      <MainNav linkToMe={me} />
-    </div>
-  );
+    <MainNav linkToMe={me} />
+  </div>
+);
 }
