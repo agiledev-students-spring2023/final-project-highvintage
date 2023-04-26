cabeb1c5cfbb16296fb24890fb31f379a0a87946
ezmartini <em4047@nyu.edu>
2023-04-26 18:54:20 -0400

    added styling to 404 not found page

--- front-end/src/pages/NotFound.js
+++ front-end/src/pages/NotFound.js
@@ -1,5 +1,18 @@
 import React from "react";
+import GenericHeader from "../components/GenericHeader";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <h1 className="text-center mt-20 text-lg">
+        {" "}
+        Oops! Page or resource not found.{" "}
+      </h1>
+      <Link className="block text-center text-sky-500 underline" to={"/home"}>
+        {" "}
+        Back to High Vintage{" "}
+      </Link>
+    </>
+  );
 }
