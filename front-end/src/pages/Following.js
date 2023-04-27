f8aedf8abde9061116acd5a9d06f6eb0b8b13aed
ezmartini <em4047@nyu.edu>
2023-04-27 19:51:06 -0400

    added error handling and load states to following

--- front-end/src/pages/Following.js
+++ front-end/src/pages/Following.js
@@ -4,11 +4,15 @@ import ProfilePreview from "../components/Profile/ProfilePreview";
 import GenericHeader from "../components/GenericHeader";
 import axios from "axios";
 import { requestURL } from "../requestURL";
+import Loading from "../components/Loading";
 
 export default function Following() {
   const [following, setFollowing] = useState([]);
   const location = useLocation();
   const { currentUser } = location.state;
+
+  const [err, setErr] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     async function fetchFollowing() {
@@ -19,21 +23,54 @@ export default function Following() {
         //const response = await axios.get('${requestURL}api/users/${props.username}/followings');
         setFollowing(response.data.following);
       } catch (error) {
-        console.error(error);
+        setErr(true);
       }
     }
 
-    fetchFollowing();
+    try {
+      fetchFollowing();
+      setLoaded(true);
+    } catch (err) {
+      setErr(true);
+    }
   }, []);
 
   const followingComponents = following.map((following) => (
     <ProfilePreview username={following.username} photo={following.photo} />
   ));
 
+  function loadPage() {
+    if (!loaded) {
+      <Loading />;
+    }
+    if (loaded && !err) {
+      if (following.length <= 0) {
+        return (
+          <p className="mt-24 text-center text-lg">
+            {" "}
+            Looks like you don't have any followers, yet!{" "}
+          </p>
+        );
+      }
+      return (
+        <div className="grid grid-cols-1 mt-12">{followingComponents}</div>
+      );
+    }
+
+    if (err) {
+      return (
+        <p className="mt-24 text-center text-lg">
+          {" "}
+          Something went wrong loading this page.
+        </p>
+      );
+    }
+  }
+
   return (
     <>
       <GenericHeader pageName="Following" />
-      <div className="grid grid-cols-1 mt-12">{followingComponents}</div>{" "}
+      {loadPage()}
     </>
   );
 }
