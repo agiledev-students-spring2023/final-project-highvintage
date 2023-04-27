b73cbcccc59c8cc3e2bb736f1f8528c5385abd8d
ezmartini <em4047@nyu.edu>
2023-04-27 19:48:04 -0400

    added load states and err handling to followers

--- front-end/src/pages/Followers.js
+++ front-end/src/pages/Followers.js
@@ -4,11 +4,14 @@ import ProfilePreview from "../components/Profile/ProfilePreview";
 import GenericHeader from "../components/GenericHeader";
 import axios from "axios";
 import { requestURL } from "../requestURL";
+import Loading from "../components/Loading";
 
 export default function Followers() {
   const [followers, setFollowers] = useState([]);
   const location = useLocation();
   const { currentUser } = location.state;
+  const [err, setErr] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     async function fetchFollowers() {
@@ -17,22 +20,52 @@ export default function Followers() {
           requestURL + "users/" + currentUser.username + "/followers"
         );
         setFollowers(response.data.followers);
+        setLoaded(true);
       } catch (error) {
-        console.error(error);
+        setErr(true);
       }
     }
-
-    fetchFollowers();
+    try {
+      fetchFollowers();
+    } catch (e) {
+      setErr(true);
+    }
   }, []);
 
   const followerComponents = followers.map((follower) => (
     <ProfilePreview username={follower.username} photo={follower.photo} />
   ));
 
+  function loadPage() {
+    if (!loaded) {
+      <Loading />;
+    }
+    if (loaded && !err) {
+      if (followers.length <= 0) {
+        return (
+          <p className="mt-24 text-center text-lg">
+            {" "}
+            Looks like you don't have any followers, yet!{" "}
+          </p>
+        );
+      }
+      return <div className="grid grid-cols-1 mt-12">{followerComponents}</div>;
+    }
+
+    if (err) {
+      return (
+        <p className="mt-24 text-center text-lg">
+          {" "}
+          Something went wrong loading this page.
+        </p>
+      );
+    }
+  }
+
   return (
     <>
       <GenericHeader pageName="Followers" />
-      <div className="grid grid-cols-1 mt-12">{followerComponents}</div>{" "}
+      {loadPage()}
     </>
   );
 }
