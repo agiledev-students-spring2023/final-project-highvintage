8fe1759bd52c77662304f1b4fd0e48d5fa3b7868
rubykiim <sk7383@nyu.edu>
2023-04-30 14:00:39 -0400

    fetchCol & fetchMe err handling

--- front-end/src/pages/OutfitCollection.js
+++ front-end/src/pages/OutfitCollection.js
@@ -50,20 +50,17 @@ export default function OutfitCollection() {
 
   useEffect(() => {
     async function fetchCollection() {
-      const response = await axios
-        .get(requestURL + "posts/collection")
-        .then(console.log("* Fetched"))
-        .catch((err) => {
-          if (err) {
-            console.log("Error :", err);
-          }
-        });
-
-      console.log("response", response);
-
-      if (response) {
-        console.log("* Fetched Posts", response.data.allPosts);
-        setAllPosts(response.data.allPosts);
+      try {
+        const response = await axios
+          .get(requestURL + "posts/collection")
+          .then(console.log("* Fetched"))
+        // console.log("response", response);
+        if (response) {
+          console.log("* Fetched Posts", response.data.allPosts);
+          setAllPosts(response.data.allPosts);
+        }
+      } catch {
+        navigate("/500")
       }
     }
     fetchCollection();
@@ -92,8 +89,13 @@ export default function OutfitCollection() {
 
   useEffect(() => {
     async function fetchMe() {
-      const response = await axios.get(requestURL + "users/me");
-      setMe(response.data.user.username);
+      try {
+        const response = await axios.get(requestURL + "users/me");
+        setMe(response.data.user.username);
+      }
+      catch {
+        navigate("/500");
+      }
     }
     fetchMe();
   });
