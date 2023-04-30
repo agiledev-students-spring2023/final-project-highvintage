e360f1fde5854eb10d57a4aadae06538b22af780
ezmartini <em4047@nyu.edu>
2023-04-30 14:28:15 -0400

    deleting console.log statements

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -27,10 +27,8 @@ router.get("/styles", async (req, res) => {
       ],
     }).save();
     let styles = fetchedStyles.styles;
-    // console.log('* styles', styles)
     res.status(201).json({ styles });
   } catch (err) {
-    console.log("Style error:", err);
     res.sendStatus(500);
   }
 });
@@ -57,12 +55,8 @@ router.post(
   upload.fields([{ name: "my_files", maxCount: 5 }]),
   async (req, res, next) => {
     const user = req.user;
-    console.log("user", user);
     const author = user.username;
-    console.log("author", author);
     const { location, content, style } = req.body;
-
-    console.log("req.files.my_files", req.files.my_files);
 
     const photos = req.files.my_files.map((file) => ({
       data: fs.readFileSync(path.join(uploadDir + "/" + file.filename)),
@@ -79,14 +73,9 @@ router.post(
         location: location,
       }).save();
 
-      console.log("photos[0]", photos[0]);
-
       if (newPost) {
-        // console.log('* newPost', newPost);
-        console.log("* date format", newPost.posted);
         db.collection("Posts").insertOne(newPost);
       } else {
-        console.log("* Failed to create post");
         res.sendStatus(500);
       }
 
@@ -101,20 +90,17 @@ router.post(
         await user.save();
         // Populate posts field in User
         const populatedUser = await User.findById(user._id).populate("posts");
-        console.log("* Populated User", populatedUser);
 
         // JUST TO MAKE EASIER TO DELETE.. IF NEEDED
         // await Post.deleteMany({});
         // remove post ids from user.posts array
         // await User.updateMany({}, { $set: { posts: [] } });
       } catch (err) {
-        console.log("* Issue saving user", err);
         res.sendStatus(500);
       }
 
       res.status(201).json({ newPost: populatedPost });
     } catch (err) {
-      console.log("Error:", err);
       res.sendStatus(500);
       next(err);
     }
@@ -123,7 +109,6 @@ router.post(
 
 // error handling middleware
 router.use((err, req, res, next) => {
-  console.error(err.stack);
   res.sendStatus(500);
 });
 
@@ -131,8 +116,6 @@ router.use((err, req, res, next) => {
 router.get("/:id/like", async (req, res) => {
   const userID = req.query.userID;
   const postID = req.params.id;
-  console.log("userID", userID);
-  console.log("postID", postID);
 
   try {
     const post = await Post.findById(postID);
@@ -142,11 +125,8 @@ router.get("/:id/like", async (req, res) => {
     // determine if it is liked
     const isLiked = post.likes.some((like) => like.equals(userID));
 
-    // console.log('numLikes from db', numLikes)
-    // console.log('isLiked from db', isLiked)
     res.json({ numLikes, isLiked });
   } catch (err) {
-    console.log("* Cannot get initial like state", err);
     res.sendStatus(500);
   }
 });
@@ -155,13 +135,9 @@ router.get("/:id/like", async (req, res) => {
 router.post("/:postID/like", async (req, res) => {
   const { userID, postID, liked, postLikes } = req.body;
   const user = req.user;
-  // console.log('userId', userID)
-  // console.log("postId", postID);
 
   let numLikes = postLikes;
   let isLiked = liked;
-  // console.log('numLikes from form', numLikes)
-  // console.log('isLiked from form', isLiked)
 
   // isLiked true = not liked, since passed in !isLiked
   if (isLiked) {
@@ -175,7 +151,6 @@ router.post("/:postID/like", async (req, res) => {
           console.log("+ Likes", post.likes.length);
         });
     } catch (err) {
-      console.log("* Error adding user to like array", err);
       res.sendStatus(500);
     }
   } else {
@@ -187,7 +162,6 @@ router.post("/:postID/like", async (req, res) => {
         console.log("- Likes", post.likes.length);
       });
     } catch (err) {
-      console.log("* Error deleting user from like array", err);
       res.sendStatus(500);
     }
   }
@@ -202,12 +176,11 @@ router.post("/:postID/like", async (req, res) => {
         isLiked = post.likes.some((like) => like.equals(user._id));
       })
       .catch((err) => {
-        console.error("* Error getting likes length", err);
+        return res.sendStatus(500);
       });
     // Return the updated number of likes and like state in the response
     res.json({ numLikes, isLiked });
   } catch (err) {
-    console.log(err);
     res.sendStatus(500);
   }
 });
@@ -218,9 +191,7 @@ router.get("/collection", async (req, res) => {
   try {
     await User.find()
       .then(async (fetchedUsers) => {
-        console.log("* fetchedUsers", fetchedUsers);
         const populatedUsers = await User.findById(user._id).populate("posts");
-        console.log("* populatedUsers", populatedUsers);
         let allPosts = [];
         [populatedUsers].forEach((user) => {
           user.posts.forEach((p) => {
@@ -232,7 +203,6 @@ router.get("/collection", async (req, res) => {
       })
       .catch((err) => console.log("* Cannot fetch all users", err));
   } catch (err) {
-    console.log(err);
     res.sendStatus(500);
   }
 });
@@ -240,12 +210,8 @@ router.get("/collection", async (req, res) => {
 // api/posts/
 router.get("/view", async (req, res) => {
   const user = req.user;
-  // console.log("user", user);
   const author = user.username;
-  // console.log("author", author);
-  // console.log("req.query", req.query);
   const postID = req.query.id;
-  // console.log("postID", postID);
 
   try {
     const foundPost = await Post.findOne({ _id: postID });
