ddb47296ec0d8b6f910d67daa9bdfc2ec0726aeb
ezmartini <em4047@nyu.edu>
2023-05-02 12:08:24 -0400

    onboarding url fix

---
+++ front-end/onboardingURL.js
@@ -0,0 +1,1 @@
+export const onboardingURL = "http://localhost:5000/";
--- front-end/src/requestURL.js
+++ front-end/src/requestURL.js
@@ -1,2 +1,1 @@
 export const requestURL = "http://localhost:5000/api/";
-export const onboardingURL = "http://localhost:5000/";
